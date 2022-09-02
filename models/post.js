ebc5ec0e3d793f1bc9d52b0747a8de5d76cfe86d
matechcide <98911237+matechcide@users.noreply.github.com>
2022-09-02 10:42:24 +0200

    correction

---
+++ models/post.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+/**
+ * @typedef {Object} postshort
+ * @property {JSON} json
+ * @property {(data: JSON)} s
+ * @property {(data: JSON)} e
+ * @property {(data: url)} r
+ */
+
+/**
+ * 
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {postshort} short
+ */
+export default function(req, res, short){
+    
+}
--- webSources/defaultScripts/toModule.js
+++ webSources/defaultScripts/toModule.js
@@ -1,10 +1,11 @@
-async function tm(action, body, label){
+async function tm(action, body, label, tempText=""){
     if(label === undefined){
         label = "labelInfo"
     }
     return await new Promise((resolve, reject) => {
         if(document.getElementById(label)){
-            document.getElementById(label).innerText = ""
+            document.getElementById(label).style.color = ""
+            document.getElementById(label).innerText = tempText
         }
         fetch(window.location.origin + "/" + loc.path[0] + "/" + action + loc.urlArgs, {
             method: "POST",
@@ -47,6 +48,7 @@ async function tm(action, body, label){
         })
         .catch((error) => {
             console.error(error);
+            reject(error)
         })
     })
 }
